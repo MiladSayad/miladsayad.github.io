23cf9d12e59d539b2d45aeedcd50b69ab412bea3
miladsayad <miladsayad@gmail.com>
2016-06-16 13:15:40 +1000

    Assignment 1 done!

--- R Workplace/Assignmnet1.R
+++ R Workplace/Assignmnet1.R
@@ -1,39 +1,53 @@
 pathFinder <- function(x, directory = 'specdata') {
-  if (x < 10) {
-    paste(directory, '/00', x, '.csv', sep = "")
-  } else if (x < 100) { 
-    paste(directory, '/0', x, '.csv', sep = "")
-  } else if (x < 333) {
-    paste(directory, '/', x, '.csv', sep = "")
-  } else {
-    print("Error: The given x is out of range.")
-  }
+    if (x < 10) {
+        paste(directory, '/00', x, '.csv', sep = "")
+    } else if (x < 100) { 
+        paste(directory, '/0', x, '.csv', sep = "")
+    } else if (x < 333) {
+        paste(directory, '/', x, '.csv', sep = "")
+    } else {
+        print("Error: The given x is out of range.")
+    }
 }
 
 pollutantmean <- function(directory, pollutant, id = 1:332) {
-  data<- NULL
-  for (i in id ) {
-    path <- pathFinder(x = i, directory = directory)
-    new <- read.csv(path,header = TRUE)
-    data <- rbind (data, new)
-  }
-  target <- paste('data', '$', pollutant, sep = '')
-  print(target)
-  round(mean(eval(parse(text = target)), na.rm = TRUE),3)
+    data <- NULL
+    for (i in id ) {
+        path <- pathFinder(x = i, directory = directory)
+        new <- read.csv(path,header = TRUE)
+        data <- rbind (data, new)
+    }
+    target.var <- paste('data', '$', pollutant, sep = '')
+    mean(eval(parse(text = target.var)), na.rm = TRUE)
 }
 
-getwd()
-pollutantmean("specdata", "sulfate", 1:10)
-
-pollutantmean("specdata", "nitrate", 70:72)
-pollutantmean("specdata", "nitrate", 23)
-
-data<- NULL
-for (i in 1:10 ) {
-  path <- pathFinder(x = i, directory = "specdata")
-  new <- read.csv(path,header = TRUE)
-  data <- rbind (data, new)
+complete <- function(directory, id = 1:332) {
+    new <- NULL
+    data <- data.frame(x=integer(), y=integer())
+    for (i in id ) {
+        path <- pathFinder(x = i, directory = directory)
+        new <- read.csv(path,header = TRUE)
+        new <- new [complete.cases(new),]
+        No <- nrow(new)
+        data <- rbind(data, c(i, No))
+    }
+    names(data) = c("id", "nobs")
+    data
 }
-data <- rbind (new, new)
-path
-mean(data$sulfate, na.rm = TRUE)
+
+corr <- function(directory , threshold = 0) {
+    new <- NULL
+    cr <- numeric()
+    for (i in 1:332 ) {
+        if (complete (directory, i)$nobs >= threshold) {
+            path <- pathFinder(x = i, directory = directory)
+            new <- read.csv(path,header = TRUE)
+            new <- new [complete.cases(new),]
+            new.cor <- cor(new$sulfate,new$nitrate)
+            if (!is.na(new.cor)){
+                cr <- c(cr, new.cor)
+            }
+        }
+    }
+    cr
+}
